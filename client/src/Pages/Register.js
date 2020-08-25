f891a9fd936f13abbf15ad7c827e4b1881d75a0c
chkim116 <66462726+chkim116@users.noreply.github.com>
2020-08-25 20:51:45 +0900

    myblog#1.1 npm i redux
    react-redux
    redux
    redux-actions
    redux-devtools-extension

--- client/src/Pages/Register.js
+++ client/src/Pages/Register.js
@@ -1,19 +1,8 @@
 import React from "react";
-import "../assets/scss/register.scss";
+import RegisterForm from "../components/RegisterForm";
 
 const Register = () => {
-  return (
-    <>
-      <form className="register__form">
-        <h2 className="register__title">회원가입</h2>
-        <input type="text" placeholder="아이디" />
-        <input type="password" min="6" placeholder="6자리 이상의 비밀번호" />
-        <input type="password" min="6" placeholder="비밀번호 확인" />
-        <input type="email" placeholder="ex) xxxxxxx@xxxx.com" />
-        <input className="form__submit" type="submit" value="가입완료" />
-      </form>
-    </>
-  );
+  return <RegisterForm></RegisterForm>;
 };
 
 export default Register;
