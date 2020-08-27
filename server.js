1bb7bbcb809943c81578129f8a88912d4d39c36d
chkim116 <66462726+chkim116@users.noreply.github.com>
2020-08-27 22:45:43 +0900

    herokuu

--- server.js
+++ server.js
@@ -6,13 +6,6 @@ import postRouter from "./src/routers/postRouter";
 dotenv.config();
 import cors from "cors";
 
-// ssr
-import fs from "fs";
-import path from "path";
-import React from "react";
-import ReactDOMServer from "react-dom/server";
-import App from "../client/src/App";
-
 const app = express();
 
 // middleware
@@ -20,33 +13,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/", postRouter);
 
-app.use("^/$", (req, res, next) => {
-  fs.readFile(
-    path.resolve("./client/build/index.html"),
-    "utf-8",
-    (err, data) => {
-      if (err) {
-        console.log(err);
-        return res.status(500).send("Some Error");
-      }
-      return res.send(
-        data.replace(
-          '<div id="root></div>',
-          `<div id="root>${ReactDOMServer.renderToString(<App />)}</div>`
-        )
-      );
-    }
-  );
-});
-
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-  });
 }
 
-app.use(express.static(path.resolve(__dirname, "..", "build")));
 app.use(cors());
 // server
 const { PORT } = process.env;
---
+++ src/models/post.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+
+const dateNow = new Date();
+
+const currentDate = `${dateNow.getFullYear()}-${
+  dateNow.getMonth() + 1
+}-${dateNow.getDate()}`;
+
+const allDate = `${currentDate}-${
+  dateNow.getHours() < 10 ? `0${dateNow.getHours()}` : `${dateNow.getHours()}`
+}시${
+  dateNow.getMinutes() < 10
+    ? `0${dateNow.getMinutes()}`
+    : `${dateNow.getMinutes()}`
+}분${
+  dateNow.getSeconds() < 10
+    ? `0${dateNow.getSeconds()}`
+    : `${dateNow.getSeconds()}`
+}초`;
+
+const PostSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: "title is required",
+  },
+  description: {
+    type: String,
+    required: "description is required",
+  },
+  tags: [String],
+  createDate: {
+    type: String,
+    default: allDate,
+  },
+});
+
+const model = mongoose.model("Post", PostSchema);
+
+export default model;
