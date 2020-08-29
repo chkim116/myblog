5ab4dd9d9ebe9681b50f3e21a71c197659eb76a3
chkim116 <66462726+chkim116@users.noreply.github.com>
2020-08-30 05:58:00 +0900

    mongo

--- db.js
+++ db.js
@@ -2,9 +2,8 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 const { MONGO_ATLAS } = process.env;
-const { MONGO_URL } = process.env;
 
-mongoose.connect(MONGO_ATLAS || MONGO_URL, {
+mongoose.connect(MONGO_ATLAS, {
   useNewUrlParser: true,
   useFindAndModify: false,
   useUnifiedTopology: true,
