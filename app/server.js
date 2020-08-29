322a42225a8af2c021b7d0a4152db669db29886f
chkim116 <66462726+chkim116@users.noreply.github.com>
2020-08-30 03:18:40 +0900

    다시

--- app/server.js
+++ app/server.js
@@ -2,9 +2,9 @@ import express from "express";
 import bodyParser from "body-parser";
 import "./db";
 import dotenv from "dotenv";
-import postRouter from "./src/routers/postRouter";
+import postRouter from "../src/routers/postRouter";
 dotenv.config();
-import "./src/models/Post";
+import "../src/models/Post";
 import cors from "cors";
 // ssr
 import fs from "fs";
@@ -12,7 +12,7 @@ import path from "path";
 import React, { useState, useCallback } from "react";
 import { Route, Switch } from "react-router-dom";
 import ReactDOMServer from "react-dom/server";
-import App from "./client/src/App";
+import App from "../client/src/App";
 
 const app = express();
 
@@ -23,7 +23,7 @@ app.use(cors());
 app.use("/", postRouter);
 app.use("^/$", (req, res, next) => {
   fs.readFile(
-    path.resolve("./client/build/index.html"),
+    path.resolve("../client/build/index.html"),
     "utf-8",
     (err, data) => {
       if (err) {
@@ -40,7 +40,7 @@ app.use("^/$", (req, res, next) => {
   );
 });
 
-app.use(express.static(path.join(__dirname + "/client/build")));
+app.use(express.static(path.join("..", "client", "build")));
 
 if (
   process.env.NODE_ENV === "production" ||
@@ -48,7 +48,7 @@ if (
 ) {
   app.use(express.static("client/build"));
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname + "/client/build/index.html"));
+    res.sendFile(path.join("..", "client", "build", "index.html"));
   });
 }
 // server
