f4be0eace3f12e6105e89fbf68f7266d92b9d6b4
chkim116 <dudnqnfqlc@naver.com>
2020-10-22 03:03:56 +0900

    경로수정

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ if (
 ) {
   app.use(express.static("./client/build"));
 
-  app.get("*", (req, res) => {
+  app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "./client", "build", "index.html"));
   });
 }
