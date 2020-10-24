b6cc2815c50a321c0136a3b625e13eff4bf42ba9
chkim116 <dudnqnfqlc@naver.com>
2020-10-25 05:02:11 +0900

    sendfile 수정

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ const cookieStore = mongoStore(session);
 app.use(express.static(path.join(__dirname, "client/build")));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+  res.sendFile(path.join(__dirname + "/client/public/index.html"));
 });
 
 // middleware
