aa471344815009882388a7a7745a9b2b2b08f61d
chkim116 <dudnqnfqlc@naver.com>
2020-10-25 13:49:49 +0900

    뺵

--- index.js
+++ index.js
@@ -42,16 +42,8 @@ const cookieStore = mongoStore(session);
 
 app.use(express.static(path.join(__dirname, "./client/build")));
 
-// app.get("/", (req, res) => {
-//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
-// });
-
-app.get("/*", (req, res) => {
-  let url = path.join(__dirname, "../client/build", "index.html");
-  if (!url.startsWith("/app/"))
-    // we're on local windows
-    url = url.substring(1);
-  res.sendFile(url);
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
 // middleware
