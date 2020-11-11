22653a9b5dde113faa8f0ef03807ec9bf324dbf1
chkim116 <dudnqnfqlc@naver.com>
2020-11-12 04:23:47 +0900

    homebanner 삭제/포스트 디테일 해쉬태그 링크

--- client/src/components/Home/HomeForm.js
+++ client/src/components/Home/HomeForm.js
@@ -1,79 +1,83 @@
 import React from "react";
-import { HomeBanner } from "./HomeBanner";
 import { HomeHashtag } from "./HomeHashtag";
 import "./HomeForm.scss";
 import { Link } from "react-router-dom";
 
 const HomeForm = ({ tagList, post }) => {
-  let tags = [];
-  tagList.map((list) => list.forEach((tag) => tags.push(tag)));
+    let tags = [];
+    tagList.map((list) => list.forEach((tag) => tags.push(tag)));
 
-  const reduceTag = tags.reduce((obj, current) => {
-    if (!obj[current]) {
-      obj[current] = 0;
-    }
-    obj[current]++;
-    return obj;
-  }, {});
-  const tagsKeyValue = Object.entries(reduceTag).sort((a, b) => b[1] - a[1]);
-  const sortTags = tagsKeyValue.map(([key, value]) => [key, value]);
+    const reduceTag = tags.reduce((obj, current) => {
+        if (!obj[current]) {
+            obj[current] = 0;
+        }
+        obj[current]++;
+        return obj;
+    }, {});
+    const tagsKeyValue = Object.entries(reduceTag).sort((a, b) => b[1] - a[1]);
+    const sortTags = tagsKeyValue.map(([key, value]) => [key, value]);
 
-  return (
-    <>
-      <HomeBanner />
-      <div className='recent'>
-        <div className='recent-post'>최근 글</div>
-        <Link to='/post' className='recent-btn'>
-          view more
-        </Link>
-      </div>
-      <div className='wrap__post'>
-        {post.slice(0, 4).map((p, index) => (
-          <div className='main__post' key={index}>
-            <Link to={`/postdetail/${p._id}`}>
-              <div className='main__post-title'>{p.title}</div>
-              <div className='main__post-desc ql-snow'>
-                <div
-                  className='ql-editor'
-                  dangerouslySetInnerHTML={{ __html: p.description }}></div>
-              </div>
-            </Link>
-          </div>
-        ))}
-      </div>
-      <main className='main'>
-        <div className='main__hash'>
-          <div className='main__hash-title'>Keyword Tags</div>
-          <div className='main__hash-box'>
-            {sortTags.map((t, index) => (
-              <HomeHashtag key={index} hash={t[0]} length={t[1]} />
-            ))}
-          </div>
-        </div>
+    return (
+        <>
+            <div className="recent">
+                <div className="recent-post">최근 글</div>
+                <Link to="/post" className="recent-btn">
+                    view more
+                </Link>
+            </div>
+            <div className="wrap__post">
+                {post.slice(0, 4).map((p, index) => (
+                    <div className="main__post" key={index}>
+                        <Link to={`/postdetail/${p._id}`}>
+                            <div className="main__post-title">{p.title}</div>
+                            <div className="main__post-desc ql-snow">
+                                <div
+                                    className="ql-editor"
+                                    dangerouslySetInnerHTML={{
+                                        __html: p.description,
+                                    }}></div>
+                            </div>
+                        </Link>
+                    </div>
+                ))}
+            </div>
+            <main className="main">
+                <div className="main__hash">
+                    <div className="main__hash-title">Keyword Tags</div>
+                    <div className="main__hash-box">
+                        {sortTags.map((t, index) => (
+                            <HomeHashtag
+                                key={index}
+                                hash={t[0]}
+                                length={t[1]}
+                            />
+                        ))}
+                    </div>
+                </div>
 
-        <div className='main__hello'>
-          <div className='main__hash-title'>Welcome to My Blog!</div>
-          <div className='main__btn'>
-            <Link to='/post'>
-              <button className='main__btn-post' type='button'>
-                POST
-              </button>
-            </Link>
-            <Link to='/guestbook'>
-              <button className='main__btn-guest' type='button'>
-                GUEST BOOK
-              </button>
-            </Link>
-            <Link to='/about'>
-              <button className='main__btn-guest' type='button'>
-                ABOUT
-              </button>
-            </Link>
-          </div>
-        </div>
-      </main>
-    </>
-  );
+                <div className="main__hello">
+                    <div className="main__hash-title">Welcome to My Blog!</div>
+                    <div className="main__btn">
+                        <Link to="/post">
+                            <button className="main__btn-post" type="button">
+                                POST
+                            </button>
+                        </Link>
+                        <Link to="/guestbook">
+                            <button className="main__btn-guest" type="button">
+                                GUEST BOOK
+                            </button>
+                        </Link>
+                        <Link to="/about">
+                            <button className="main__btn-guest" type="button">
+                                ABOUT
+                            </button>
+                        </Link>
+                    </div>
+                </div>
+            </main>
+        </>
+    );
 };
 
 export default HomeForm;
--- client/src/components/Posting/PostDetailForm.js
+++ client/src/components/Posting/PostDetailForm.js
@@ -5,80 +5,80 @@ import { PostComment } from "./PostComment";
 import { PostRecentForm } from "./PostRecentForm";
 
 const PostDetailForm = ({
-  post,
-  username,
-  onClick,
-  onChangeComment,
-  onComment,
-  onDelComment,
-  location,
-  onViewMore,
-  onViewClose,
-  count,
-  fakeComment,
-  commentValue,
-  admin,
-  allPost,
+    post,
+    username,
+    onClick,
+    onChangeComment,
+    onComment,
+    onDelComment,
+    location,
+    onViewMore,
+    onViewClose,
+    count,
+    fakeComment,
+    commentValue,
+    admin,
+    allPost,
 }) => {
-  const { title, description, _id, createDate, tags, comment } = post;
+    const { title, description, _id, createDate, tags, comment } = post;
 
-  return (
-    <>
-      <div className='post__detail' key={_id}>
-        <div
-          className='back'
-          onClick={() => {
-            window.history.back();
-          }}>
-          뒤로가기
-        </div>
-        <div className='post__btn'>
-          {admin && (
-            <>
-              <span className='btn'>
-                <Link to={`/edit/${_id}`}>Edit</Link>
-              </span>
-              <span className='btn' onClick={onClick}>
-                Delete
-              </span>
-            </>
-          )}
-        </div>
-        <div className='post__detail-title'>{title}</div>
-        <div className='post__detail-desc ql-snow'>
-          <div
-            className='ql-editor'
-            dangerouslySetInnerHTML={{ __html: description }}></div>
-        </div>
+    return (
+        <>
+            <div className="post__detail" key={_id}>
+                <div
+                    className="back"
+                    onClick={() => {
+                        window.history.back();
+                    }}>
+                    뒤로가기
+                </div>
+                <div className="post__btn">
+                    {admin && (
+                        <>
+                            <span className="btn">
+                                <Link to={`/edit/${_id}`}>Edit</Link>
+                            </span>
+                            <span className="btn" onClick={onClick}>
+                                Delete
+                            </span>
+                        </>
+                    )}
+                </div>
+                <div className="post__detail-title">{title}</div>
+                <div className="post__detail-desc ql-snow">
+                    <div
+                        className="ql-editor"
+                        dangerouslySetInnerHTML={{ __html: description }}></div>
+                </div>
 
-        <div className='post__detail-tags'>
-          {tags.map((tg, index) => (
-            <span key={index} data-tag={tg}>
-              #{tg}
-            </span>
-          ))}
-        </div>
-        <span className='post__detail-tags'></span>
-        <div className='post__detail-date'>Uploaded: {createDate}</div>
-        <PostComment
-          comment={comment}
-          username={username}
-          onChangeComment={onChangeComment}
-          onComment={onComment}
-          onDelComment={onDelComment}
-          commentValue={commentValue}
-          fakeComment={fakeComment}
-        />
-        <PostRecentForm
-          onViewMore={onViewMore}
-          onViewClose={onViewClose}
-          allPost={allPost}
-          location={location}
-          count={count}
-        />
-      </div>
-    </>
-  );
+                <div className="post__detail-tags">
+                    {tags.map((tg, index) => (
+                        <span key={index} data-tag={tg}>
+                            <Link to={`/search?tag=${tg}`}>#{tg}</Link>
+                        </span>
+                    ))}
+                </div>
+                <span className="post__detail-tags"></span>
+                <div className="post__detail-date">Uploaded: {createDate}</div>
+                <PostComment
+                    comment={comment}
+                    username={username}
+                    onChangeComment={onChangeComment}
+                    onComment={onComment}
+                    onDelComment={onDelComment}
+                    commentValue={commentValue}
+                    fakeComment={fakeComment}
+                />
+                <PostRecentForm
+                    onViewMore={onViewMore}
+                    onViewClose={onViewClose}
+                    allPost={allPost}
+                    location={location}
+                    count={count}
+                />
+            </div>
+        </>
+    );
 };
 
 export default PostDetailForm;
