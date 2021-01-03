150446462e1cfce926f00fb467d0ec4a0fe353d5
chkim116 <dudnqnfqlc@naver.com>
2021-01-03 21:32:48 +0900

    Fix : 모바일 버전 홈 css 수정

--- client/src/components/Home/HomeHashtag.js
+++ client/src/components/Home/HomeHashtag.js
@@ -2,12 +2,12 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export const HomeHashtag = ({ hash, length }) => {
-  const color = `#${Math.round(Math.random() * 0xffffff).toString(16)}`;
-  return (
-    <Link to={`/search?tag=${hash}`}>
-      <div className='main__hash-tag' data-tag={hash} style={{ color }}>
-        #{hash}({length})
-      </div>
-    </Link>
-  );
+    const color = `#${Math.round(Math.random() * 0xfdfdfd).toString(16)}`;
+    return (
+        <Link to={`/search?tag=${hash}`}>
+            <div className="main__hash-tag" data-tag={hash} style={{ color }}>
+                #{hash}({length})
+            </div>
+        </Link>
+    );
 };
