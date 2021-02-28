dd5128fb0ff991d617a917761abc2171d114e298
kimchanghoe <cskim@veluga.kr>
2021-02-28 18:50:47 +0900

    Feat : 404 페이지 추가

--- nextclient/components/layouts/AppEmpty.tsx
+++ nextclient/components/layouts/AppEmpty.tsx
@@ -2,11 +2,15 @@ import { Button, Empty } from "antd"
 import React from "react"
 import AppContents from "./AppContents"
 
-const AppEmpty = () => {
+const AppEmpty = ({
+    text = "죄송하지만.. 게시글을 못불러 왔네요..",
+}: {
+    text?: string
+}) => {
     return (
         <AppContents>
             <div style={{ textAlign: "center", lineHeight: 10 }}>
-                <Empty description="죄송하지만.. 게시글을 못불러 왔네요.."></Empty>
+                <Empty description={text}></Empty>
                 <Button type="primary" onClick={() => history.back()}>
                     뒤로가기
                 </Button>
