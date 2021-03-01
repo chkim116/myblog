37549284bea6a463be080a42c3ff7e6d7f2d24b3
kimchanghoe <cskim@veluga.kr>
2021-03-01 14:36:14 +0900

    Feat : sitemap 색인 생성

--- nextclient/next.config.js
+++ nextclient/next.config.js
@@ -1,4 +1,10 @@
 module.exports = {
     // Target must be serverless
     target: "serverless",
+    webpack: (config, { isServer }) => {
+        if (isServer) {
+            require("./scripts/sitemap.js")
+        }
+        return config
+    },
 }
